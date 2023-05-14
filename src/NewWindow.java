c407cd930b1a29ca533a8189b2d748972c6d5842
Vu Minh Chien <57214915+ChienVuHp123@users.noreply.github.com>
2023-05-14 18:24:35 +0700

    Update NewWindow.java

--- src/NewWindow.java
+++ src/NewWindow.java
@@ -2,17 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class NewWindow {
-    JFrame frame = new JFrame();
-    JLabel label = new JLabel();
+    JFrame frame = new JFrame(); // tạo ra graphic window
+    JLabel label = new JLabel(); // hiện thị văn bản trên window
 
    public NewWindow() {
-        label.setBounds(0,0,100,50);
-        label.setFont(new Font(null, Font.PLAIN,25));
+        label.setBounds(0,0,100,50); // đạt vị trí và kích thước của label
+        label.setFont(new Font(null, Font.PLAIN,25));  // set kiểu Font cho label
         frame.add(label);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //thiết lập hành vi mặc định khi đóng cửa sổ.
+        frame.setSize(420, 420); // đăt kích thước của sổ
         frame.setLayout(null);
-        frame.setVisible(true);
+        frame.setVisible(true); // hiển thị của sổ lên màn hình
     }
 }
